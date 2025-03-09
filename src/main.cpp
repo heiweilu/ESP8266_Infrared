9cb8721f35709570ff01258d39e94cba50a05d2c
heiweilu <273925452@qq.com>
2025-03-09 22:50:43 +0800

    Update platformio.ini and main.cpp
    - Modified platformio.ini to adjust project configuration.
    - Updated src/main.cpp to implement new features or fix issues.
    
    Signed-off-by: heiweilu <273925452@qq.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,16 @@
 #include <Arduino.h>
-#include <Adafruit_NeoPixel.h>
 
-#define PIN D4
-#define NUMPIXELS 8
-Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+#define LED_PIN 2              
 
-
-
-
-void setup() {
-    pixels.begin();
+void setup()
+{
+    pinMode(LED_PIN, OUTPUT);  
 }
 
-void loop() {
-    pixels.clear();
-
-    for (int i = 0; i < NUMPIXELS; i++)
-    { // For each pixel...
-
-        // pixels.Color() takes RGB values, from 0,0,0 up to 255,255,255
-        // Here we're using a moderately bright green color:
-        pixels.setPixelColor(i, pixels.Color(0, 150, 0));
-
-        pixels.show(); // Send the updated pixel colors to the hardware.    
-    }
-    delay(1000); // Pause before next pass through loop
+void loop()
+{
+    digitalWrite(LED_PIN, LOW);  
+    delay(1000);                 
+    digitalWrite(LED_PIN, HIGH); 
+    delay(1000);                
 }
-
